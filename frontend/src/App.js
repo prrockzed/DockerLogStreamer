4eb5daae58015d9781f33ecd9d56f655ee93633f
prrockzed <priyanshuranjan2806@gmail.com>
2024-12-20 20:56:28 +0530

    Made logviewer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 // src/App.js
 import React from "react";
 import Navbar from "./components/Navbar";
+import LogViewer from "./components/LogViewer";
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <LogViewer />
     </div>
   );
 };
